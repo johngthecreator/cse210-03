4be35e696e5fd18e93c3cc16f717d5029647b758
JT Clarke <jacksonclarke2001@hotmail.com>
2022-10-18 21:27:14 -0600

    check user input implemented

--- Game/Jumper.cs
+++ Game/Jumper.cs
@@ -1,19 +1,20 @@
 namespace Unit3.Game{
     class Jumper{
         // Keep tack of score, get input from user.
-        public void readUserInput(){            
+        public void readUserInput(){ 
+        Word check = new Word();
             // Ask user to guess a letter
             Console.Write("Guess a letter [a-z]:");
 
             try{
                 char input = Console.ReadLine()[0];
+                check.checkUserInput(input);
             }
-            catch(Exception e){
-                
-                char input;
+            catch(Exception e){    
+                check.checkUserInput('0');
             }
+
             
-            //checkUserInput(input);
             
         }
     }
