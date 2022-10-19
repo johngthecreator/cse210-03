532d904e8a01653249943fd6893521d267a26dba
JT Clarke <jacksonclarke2001@hotmail.com>
2022-10-18 21:21:34 -0600

    readUserInput function

--- Game/Jumper.cs
+++ Game/Jumper.cs
@@ -1,6 +1,20 @@
 namespace Unit3.Game{
     class Jumper{
         // Keep tack of score, get input from user.
+        public void readUserInput(){            
+            // Ask user to guess a letter
+            Console.Write("Guess a letter [a-z]:");
 
+            try{
+                char input = Console.ReadLine()[0];
+            }
+            catch(Exception e){
+                
+                char input;
+            }
+            
+            //checkUserInput(input);
+            
+        }
     }
 }
